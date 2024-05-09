0d9b01a3696a2fcaf8e9ecdfc20ffcea7aaf0ba8
ARFprince <parmanurrahmanfaisal@gmail.com>
2024-05-09 14:58:53 +0600

    finished

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const allImages=document.querySelector("#container");
+const allImages=document.querySelector("#catList");
+const form=document.querySelector("#form");
+const inputElement=document.querySelector("#search-input");
+const showMore=document.querySelector("#more");
+let page=1;
+let searchValue;
+
 async function main(){
     try {
-        const response= await fetch("https://api.unsplash.com/search/photos/?client_id=VhdYhjzQtbZIaZol7AdkWYdvOsnKGdb_xdIn3tWyNSk&query=cat",);
+        const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=VhdYhjzQtbZIaZol7AdkWYdvOsnKGdb_xdIn3tWyNSk&query=${searchValue}&page=${page}`,);
         const x = await response.json();
-
-        for (const result of x.results) {
-            const newItem=document.createElement("div");
+        for (const item of x.results) {
             const imageElemnt=document.createElement("img");
-            imageElemnt.src=result;
-            console.log(imageElemnt);
-            newItem.appendChild(imageElemnt);
-            allImages.appendChild(newItem);
+            imageElemnt.classList.add("imageStyle");
+            imageElemnt.src=item.urls.full;
+
+            const newDiv=document.createElement("div");
+            newDiv.classList.add("newDiv");
+
+            newDiv.appendChild(imageElemnt);
+            allImages.appendChild(newDiv);
+
+            showMore.classList.remove("hidden");
         }
     } catch (error) {
-        console.log("here is ur eror : ", error);
+        console.log("here is ur error : ", error);
     }
 }
 
-main();
+form.addEventListener("submit", async (e)=>{
+    e.preventDefault();
+    searchValue=inputElement.value;
+    main();
+    allImages.innerHTML="";
+});
+
+showMore.addEventListener("click",()=>{
+    page++;
+    main();
+});
